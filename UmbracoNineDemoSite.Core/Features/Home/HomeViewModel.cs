6d1ff136d1a2315060659ce11198a402e481bd94
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2021-07-02 15:53:37 +0200

    a little cleaner code for Hero?

--- UmbracoNineDemoSite.Core/Features/Home/HomeViewModel.cs
+++ UmbracoNineDemoSite.Core/Features/Home/HomeViewModel.cs
@@ -1,18 +1,33 @@
 ﻿using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Extensions;
+using UmbracoNineDemoSite.Core.Features.Shared.Components.Hero;
 using UmbracoNineDemoSite.Core.Features.Shared.Constants;
 using UmbracoNineDemoSite.Core.Features.Shared.Content;
 
 namespace UmbracoNineDemoSite.Core.Features.Home
 {
-    public class HomeViewModel : SitePageBase, IHeadingPage
-    {
-        public HomeViewModel(IPublishedContent content) : base(content) {}
+	public class HomeViewModel : SitePageBase, IHeadingPage
+	{
+		public HomeViewModel(IPublishedContent content) : base(content) { }
 
-        public string Heading => this.Content.Value<string>(PropertyAlias.Heading);
-        public string Preamble => this.Content.Value<string>(PropertyAlias.Preamble);
-        public string BackgroundImage => this.Content.Value<string>(PropertyAlias.BackgroundImage);
-        public string CallToActionUrl => this.Content.Value<IPublishedContent>(PropertyAlias.CallToActionUrl)?.Url();
-        public string CallToActionLabel => this.Content.Value<string>(PropertyAlias.CallToActionLabel);
-    }
+		public string Heading => this.Content.Value<string>(PropertyAlias.Heading);
+		public string Preamble => this.Content.Value<string>(PropertyAlias.Preamble);
+		public string BackgroundImage => this.Content.Value<string>(PropertyAlias.BackgroundImage);
+		public string CallToActionUrl => this.Content.Value<IPublishedContent>(PropertyAlias.CallToActionUrl)?.Url();
+		public string CallToActionLabel => this.Content.Value<string>(PropertyAlias.CallToActionLabel);
+		public HeroViewModel Hero
+		{
+			get
+			{
+				return new HeroViewModel()
+				{
+					Heading = Heading,
+					Preamble = Preamble,
+					BackgroundImageUrl = BackgroundImage,
+					CallToActionUrl = CallToActionUrl,
+					CallToActionLabel = CallToActionLabel
+				};
+			}
+		}
+	}
 }
