dd0e8037b7a7198f05bdb17007025f1fdfef2470
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2021-07-08 13:12:31 +0200

    cleaner code?

--- UmbracoNineDemoSite.Core/Features/Shared/Components/Hero/HeroViewComponent.cs
+++ UmbracoNineDemoSite.Core/Features/Shared/Components/Hero/HeroViewComponent.cs
@@ -2,8 +2,15 @@
 
 namespace UmbracoNineDemoSite.Core.Features.Shared.Components.Hero
 {
-	[ViewComponent(Name = "Hero")]
-	public class HeroComponent : ViewComponent
+	//The following two changes simplify the code according to https://docs.microsoft.com/en-us/aspnet/core/mvc/views/view-components?view=aspnetcore-5.0
+	// A view component class can be created by any of the following:
+	//	- Deriving from ViewComponent
+	//	- Decorating a class with the[ViewComponent] attribute, or deriving from a class with the[ViewComponent] attribute
+	//	- Creating a class where the name ends with the suffix ViewComponent
+
+	//[ViewComponent(Name = "Hero")]
+	//public class HeroComponent : ViewComponent
+	public class Hero : ViewComponent
 	{
 		public IViewComponentResult Invoke(HeroViewModel heroViewModel)
 		{
