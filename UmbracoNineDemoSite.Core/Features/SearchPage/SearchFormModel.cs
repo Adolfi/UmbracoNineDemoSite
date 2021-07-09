7a0826ecd44e5fd3befeaa2f52447ec0a38bf4e6
Dennis Adolfi <dennisadolfi@gmail.com>
2021-07-09 15:09:13 +0200

    Merged and updated search form

--- UmbracoNineDemoSite.Core/Features/SearchPage/SearchFormModel.cs
+++ UmbracoNineDemoSite.Core/Features/SearchPage/SearchFormModel.cs
@@ -8,8 +8,8 @@ namespace UmbracoNineDemoSite.Core.Features.SearchPage
         public string TotalResults { get; set; }
         public string NoResultsFound { get; set; }
         public string SearchTermText { get; set; }
-        public int Skip { get; set; }
-        public int Take { get; set; }
+        public int Skip => 0; // This is here for future improvment with pagination.
+        public int Take => 100; // This is here for future improvment with pagination.
         public SearchResults SearchResults { get; set; }
     }
 }
